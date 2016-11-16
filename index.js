5317b541663d3b042e6c1b1e1d01fb94f1dd6bf4
Sampath Sitinamaluwa <sampathsris@gmail.com>
2016-11-16 16:39:42 +0530

    Improvements to output format

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 
 const fs = require('fs');
 
-module.exports = JSON.parse(require('fs').readFileSync('list.json', 'utf8'));
+const RADIX = 10;
+
+let list = JSON.parse(fs.readFileSync('list.json', 'utf8'));
+
+list.leap_seconds = list.leap_seconds.map((entry) => {
+    return {
+        timestamp: parseInt(entry[0], RADIX),
+        offset: parseInt(entry[1], RADIX)
+    }
+});;
+
+list.last_updated = parseInt(list.last_updated, RADIX);
+list.expiration_date = parseInt(list.expiration_date, RADIX);
+
+module.exports = list;
